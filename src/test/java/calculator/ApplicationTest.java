81462c9b70ec327ecf4925d4b5744131303ffc3c
jun562 <capboy290@naver.com>
2025-10-17 22:39:45 +0900

    test(unit): 단잍 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    //    성공 케이스
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,13 +18,157 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 엔터만_입력(){
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 탭만_입력(){
+        assertSimpleTest(() -> {
+            run("\t");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 스페이스만_입력(){
+        assertSimpleTest(() -> {
+            run(" ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 양수만_입력(){
+        assertSimpleTest(() -> {
+            run("3");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 구분자만_입력(){
+        assertSimpleTest(() -> {
+            run(",:");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 공백_포함된_숫자_입력(){
+        assertSimpleTest(() -> {
+            run(" 3\t");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 공백_포함된_문자열_입력(){
+        assertSimpleTest(() -> {
+            run(" 3 \t,2");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 연속된_구분자_포함된_문자열_입력(){
+        assertSimpleTest(() -> {
+            run("2,,:2");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_숫자인_문자열_입력1(){
+        assertSimpleTest(() -> {
+            run("//2\\n324");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_숫자인_문자열_입력2(){
+        assertSimpleTest(() -> {
+            run("//2\\n2,2");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_0인_문자열_입력(){
+        assertSimpleTest(() -> {
+            run("//0\\n102");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+    @Test
+    void 커스텀_구분자가_공백인_문자열_입력(){
+        assertSimpleTest(() -> {
+            run("// \n2 3,3");
+            assertThat(output()).contains("결과 : 8");
+        });
+    }
+
+//    실페 케이스 (Exception)
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    @Test
+    void 음수_포함된_문자열_입력(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-2:3,4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_문자가_아닌_0이_포함(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0:3,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자_미포함(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("3 2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_문자가_아닌_문자가_포함(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1#2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀할_문자가_미포함(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀할_문자가_문자열(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//&&\\n3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});

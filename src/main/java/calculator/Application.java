cbb7e37f3a50608e6af0af4dd0bb1641368053da
jun562 <capboy290@naver.com>
2025-10-19 18:36:27 +0900

    feat(main): 전체 애플리케이션 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            Delimiter delimiter = new Delimiter();
+            Parser parser = new Parser(delimiter);
+            Validator validator = new Validator();
+            Converter converter = new Converter();
+            Adder adder = new Adder();
+
+            String input = Input.receiveInput();
+
+            String[] parts = parser.parse(input);
+
+            validator.validate(parts);
+
+            List<Integer> numbers = converter.convertParts(parts);
+
+            int result = adder.addParts(numbers);
+            
+            Output.printResult(result);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }

204a638f78dd576d1cd9e07a75f3542bb1953062
jun562 <capboy290@naver.com>
2025-10-19 18:54:57 +0900

    fix(main): 테스트 코드에 따른 try-catch 문 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,28 +5,22 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            Delimiter delimiter = new Delimiter();
-            Parser parser = new Parser(delimiter);
-            Validator validator = new Validator();
-            Converter converter = new Converter();
-            Adder adder = new Adder();
+        Delimiter delimiter = new Delimiter();
+        Parser parser = new Parser(delimiter);
+        Validator validator = new Validator();
+        Converter converter = new Converter();
+        Adder adder = new Adder();
 
-            String input = Input.receiveInput();
+        String input = Input.receiveInput();
 
-            String[] parts = parser.parse(input);
+        String[] parts = parser.parse(input);
 
-            validator.validate(parts);
+        validator.validate(parts);
 
-            List<Integer> numbers = converter.convertParts(parts);
+        List<Integer> numbers = converter.convertParts(parts);
 
-            int result = adder.addParts(numbers);
-            
-            Output.printResult(result);
+        int result = adder.addParts(numbers);
 
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+        Output.printResult(result);
     }
 }

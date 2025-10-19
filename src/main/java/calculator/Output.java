75e38b4896e7df8901f71c9adfd09133e5595a1e
jun562 <capboy290@naver.com>
2025-10-19 18:31:02 +0900

    feat(output): 계산 결과 출력 로직 구현

---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Output {
+    private Output() {
+    }
+
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+}

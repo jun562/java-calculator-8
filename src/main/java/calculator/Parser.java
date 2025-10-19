c29b01847aeec5328a401a656cc35c7ece7aad7c
jun562 <capboy290@naver.com>
2025-10-19 16:41:24 +0900

    feat(parser): 문자열 배열 요소의 공백 문자 제거 로직 추가

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -14,18 +14,30 @@ public class Parser {
 
     public String[] parse(String input) {
         String processedInput = delimiter.extractCustomDelimiter(input);
-        List<String> allDelimiters = delimiter.getDelimiters();
 
+        List<String> allDelimiters = delimiter.getDelimiters();
         String regex = buildRegex(allDelimiters);
-        return processedInput.split(regex);
+
+        String[] parts = processedInput.split(regex);
+
+        return trimParts(parts);
     }
 
     private String buildRegex(List<String> delimiters) {
-        // 스트림 생성
+        // 스트림 생성 (List)
         Stream<String> delimiterStream = delimiters.stream();
         // 특수문자 이스케이프 처리
         Stream<String> quotedDelimiters = delimiterStream.map(Pattern::quote);
         // "|" 로 Regex 생성
         return quotedDelimiters.collect(Collectors.joining("|"));
+    }
+
+    private String[] trimParts(String[] parts) {
+        // 스트림 생성 (Array)
+        Stream<String> partStream = Stream.of(parts);
+        // 각 요소 공백 제거
+        Stream<String> trimmedStream = partStream.map(String::trim);
+        // 문자열 배열로 변환
+        return trimmedStream.toArray(String[]::new);
     }
 }
